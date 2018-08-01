deb4ce77e9b59bdbfa3503175ef660a30ad7daf5
Teddy <lunlun414@gmail.com>
2018-08-02 00:19:39 +0800

    init

---
+++ client/main.js
@@ -0,0 +1,11 @@
+const socket = new WebSocket('ws://localhost:8080')
+socket.onopen = function () {
+  console.log('Connected')
+  socket.send(JSON.stringify({
+    event: 'events',
+    data: 'test'
+  }))
+  socket.onmessage = function (data) {
+    console.log(data)
+  }
+}
